f8b4c24a8c95087901c1c112e1451fccb040aad6
cheng87126 <cheng87126@gmail.com>
2018-08-19 23:45:46 +0800

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,38 @@ import sty from './app.scss'
 class App extends React.Component{
 	constructor() {
 		super()
+		this.state = {
+			prodList:[]
+		}
+		fetch('https://easy-mock.com/mock/5b7984a3bcb2ab748b0c2559/product')
+			.then((res)=>res.json())
+			.then((data)=>{
+				this.setState({
+					prodList:data.data
+				})
+			})
 	}
 	render(){
+		let {prodList} = this.state
 		return (
 			<section className={sty.product}>
-				<a href="">
-					<img src="https://via.placeholder.com/100x150" alt="img" />
-					<p>
-						名称描述
-					</p>
-					<p>
-						<span className={sty.price}>¥99</span>
-						<span className={sty.original}>¥99</span>
-					</p>
-					<p className="btn">立即购买</p>
-				</a>
-				<a href="">
-					<img src="https://via.placeholder.com/100x150" alt="img" />
-					<p>
-						名称描述
-					</p>
-					<p>
-						<span className={sty.price}>¥99</span>
-						<span className={sty.original}>¥99</span>
-					</p>
-					<p className="btn">立即购买</p>
-				</a>
+				{
+					prodList.map((item,idx)=>{
+						return (
+							<a href="" key={idx}>
+								<img src={item.img} alt="img" />
+								<p>
+									{item.name}{item.detail}
+								</p>
+								<p>
+									<span className={sty.price}>¥{item.price}</span>
+									<span className={sty.original}>¥{item.original}</span>
+								</p>
+								<p className="btn">立即购买</p>
+							</a>
+						)
+					})
+				}
 			</section>
 		)
 	}
